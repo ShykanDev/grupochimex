4c81792ce4a82553d992ed36890726df8032e9e5
Shykan <shykadev@gmail.com>
2025-02-08 02:00:48 -0600

    UI changes

---
+++ src/components/WebsiteCard.vue
@@ -0,0 +1,130 @@
+<template>
+  <!-- Card horizontal -->
+  <div v-if="!showCardSquare"
+    class="max-w-full p-4 mx-auto animate-fade-up bg-white shadow-lg rounded-xl hover:shadow-2xl transition-all ease-in-out duration-300 hover:border-2 hover:border-sky-500 group">
+    <div class="flex gap-6 h-72"> <!-- Contenedor horizontal -->
+      <!-- Imagen -->
+      <div class="w-1/3 h-full overflow-hidden rounded-xl shrink-0">
+        <img :src="website.domainImgUrl" alt="Domain Preview"
+          class="object-cover w-full h-full transition-transform duration-300 group-hover:scale-105"
+          onerror="this.src='https://via.placeholder.com/800x400/EAEAEA/808080?text=No+Image'">
+      </div>
+
+      <!-- Contenido -->
+      <div class="flex flex-col flex-1 py-2">
+        <!-- Encabezado -->
+        <div class="flex items-center justify-between mb-3">
+          <h2 class="text-2xl font-bold text-gray-800 font-poppins">{{ currentDomain }}</h2>
+          <span class="px-3 py-1 text-sm font-semibold text-green-700 bg-green-100 rounded-full">Activo</span>
+        </div>
+
+        <!-- Descripción -->
+        <p class="flex-1 mb-4 text-gray-600 line-clamp-4">
+          {{ website.domainDescription }}
+        </p>
+
+
+
+        <!-- Botón -->
+        <a :href="website.websiteUrl" target="_blank"
+          class="flex items-center justify-between px-6 py-3 transition-colors bg-sky-500 rounded-lg hover:bg-sky-600 group-button">
+          <span class="font-medium text-white">Visitar Sitio</span>
+          <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <!-- Card cuadrada -->
+  <div v-if="showCardSquare"
+    class="max-w-xl p-1 mx-auto animate-fade-up  bg-white text-slate-700 shadow-lg rounded-xl hover:shadow-2xl hover:scale-[1.02] hover:border-2 hover:border-orange-600 transition-all ease-in-out duration-300 hover:bg-orange-50 hover:text-slate-800 ">
+    <!-- Imagen superior -->
+    <img :src="website.domainImgUrl" alt="Domain Preview" class="w-full rounded-2xl"
+      onerror="this.src='https://via.placeholder.com/800x400/EAEAEA/808080?text=No+Image'">
+
+    <!-- Contenido -->
+    <div class="p-6">
+      <!-- Encabezado -->
+      <div class="flex items-start justify-between mb-4">
+        <h2 v-if="website.domainName.length == 1" class="text-2xl font-bold ">{{ currentDomain }}</h2>
+        <h2 v-else class="text-2xl font-bold animate-fade-up" :key="dynamicDomainName">{{ dynamicDomainName }}</h2>
+
+        <span class="bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded-full">Activo</span>
+      </div>
+
+      <!-- Información -->
+      <div class="space-y-3">
+        <div class="flex items-center">
+          <!-- <svg class="w-5 h-5 mr-2 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z">
+            </path>
+          </svg> -->
+          <!-- <p class="text-gray-600">Categoría: <span class="ml-1 ">{{ website.category }}</span></p> -->
+        </div>
+      </div>
+
+      <!-- Descripción -->
+      <p class="mt-4 text-lg  font-poppins">
+        {{ website.domainDescription }}
+      </p>
+
+      <!-- Botón -->
+      <div class="mt-6">
+        <a :href="website.websiteUrl" target="_blank"
+          class="flex items-center justify-center px-4 py-2 text-white transition-colors duration-200 bg-orange-600 rounded-lg hover:bg-orange-700">
+          <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1">
+            </path>
+          </svg>
+          Visitar Sitio
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, ref } from 'vue';
+interface IWebsite {
+  domainName: string[];
+  domainDescription: string;
+  domainImgUrl: string;
+  category: string;
+  creationDate: string;
+  status: string;
+  websiteUrl: string;
+}
+const props = defineProps({
+  website: {
+    type: Object as () => IWebsite,
+    required: true
+  },
+  showCardSquare: {
+    type: Boolean,
+    default: true
+  }
+})
+
+// Due to $website prop has two ways to get the url(s), array if website has multiples domains (website.domainName) and website.websiteUrl if only has 1 domain, this function replaces (deletes) the "https:/"  when its only one domain
+const currentDomain = computed(() => (props.website.domainName.length > 1) ? props.website.domainName[0] : props.website.websiteUrl.replace(/https:\/\//gi, ''));
+
+// dinamyc variable that stores the domain name based on the lenght of the domainName array
+const dynamicDomainName = ref(props.website.domainName[0]);
+
+// function to toggle the domainName aleatry based on the length of the domainName array
+const toggleDomainName = () => {
+  setInterval(() => {
+    dynamicDomainName.value = props.website.domainName[Math.floor(Math.random() * props.website.domainName.length)];
+  }, 2000);
+};
+
+onMounted(() => {
+  toggleDomainName();
+});
+
+</script>
+
+<style scoped></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- Header -->
+    <header class="sticky top-0 z-50 shadow-md bg-white">
+      <div class="container mx-auto px-6 py-3 flex items-center justify-between">
+        <a href="#"
+          class="flex items-center space-x-2 text-xl font-bold font-urbanist text-gray-800 transition-colors rounded-md hover:text-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <img src="@/assets/IndoExportsLogoFinal.svg" alt="" class="w-10 h-10">
+          <div class="text-base">
+            <span class="text-sky-900">www</span>
+            <span class="text-sky-950">.</span>
+            <span class="text-sky-600">grupo</span>
+            <span class="text-slate-800">chimex</span>
+            <span class="text-sky-700">.</span>
+            <span class="text-sky-900">com</span>
+          </div>
+        </a>
+
+        <nav class="hidden md:flex space-x-6">
+          <a href="#" class="font-medium text-orange-600 hover:text-blue-500 transition-colors duration-200">Inicio</a>
+          <a href="#"
+            class="font-medium text-orange-600 hover:text-blue-500 transition-colors duration-200">Contacto</a>
+        </nav>
+
+        <details class="md:hidden">
+          <summary class="cursor-pointer">
+            <svg class="w-6 h-6 text-gray-700 hover:text-blue-500 transition-colors duration-200" fill="none"
+              stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </summary>
+          <nav
+            class="mt-2 flex flex-col space-y-3 bg-white shadow-md rounded-md p-4 absolute right-0 top-full w-64 z-10">
+            <a href="#"
+              class="font-medium text-gray-700 hover:text-blue-500 transition-colors duration-200 block">Inicio</a>
+            <a href="#"
+              class="font-medium text-gray-700 hover:text-blue-500 transition-colors duration-200 block">Contacto</a>
+          </nav>
+        </details>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <main class="flex-1">
+      <slot name="main"></slot>
+    </main>
+
+    <!-- Footer -->
+    <footer class="px-6 py-8 text-white bg-orange-500 md:px-12">
+      <div class="grid gap-8 mb-8 md:grid-cols-2">
+
+
+        <div>
+          <h3 class="mb-4 text-xl font-bold">Contacto</h3>
+          <ul class="space-y-2 text-white">
+            <li class="flex items-center space-x-2">
+              <i class="fas fa-phone"></i>
+              <a href="tel:+525563950179" class="text-white">
+                <span>+52 55 6395 0179</span>
+              </a>
+            </li>
+            <li class="flex items-center space-x-2">
+              <i class="fas fa-envelope"></i>
+              <a href="mailto:informes@grupochimex.com" class="text-white">
+                <span>informes@grupochimex.com</span>
+              </a>
+            </li>
+            <li class="flex items-center space-x-2">
+              <i class="fas fa-map-marker-alt"></i>
+              <span>Gutenberg #128 Anzures, Miguel Hidalgo, 11590 Ciudad de México</span>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="mb-4 text-xl font-bold">Mapa</h3>
+          <div class="flex space-x-4">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15050.158369675277!2d-99.17607!3d19.432289!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8b32758939b%3A0xf34fbd07bc47d6dd!2sGutenberg%20128%2C%20Anzures%2C%20Miguel%20Hidalgo%2C%2011590%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1sen!2smx!4v1738996784058!5m2!1sen!2smx"
+              width="600" height="200" style="border:0;" allowfullscreen="false" loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-6 text-center borderwhiteder-gray-700 text-gray-50">
+        <p>&copy; 2024 Grupo Chimex. Todos los derechos reservados.</p>
+        <p class="mt-1 text-xs">Diseño por <small class="text-sm italic">PACA</small> <i
+            class="text-sky-800 fas fa-tornado"></i></p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// No se requiere lógica adicional para este ejemplo
+</script>
+
+<style scoped>
+/* Puedes añadir estilos personalizados aquí si es necesario */
+</style>
