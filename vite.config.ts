bf9fb483ccabdd7608244ddd9d6138cfcb0e70df
Chemex <chemex@MacBook-Pro-de-Chemex.local>
2025-02-17 17:19:54 -0600

    added fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: '/grupochimex/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
